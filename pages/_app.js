829973e508eac753c8518f8813a62decd38e3d89
Austin <osengaa10@icloud.com>
2021-11-01 15:18:22 -0400

    fixed routing refresh 404 error

--- pages/_app.js
+++ pages/_app.js
@@ -5,25 +5,35 @@ import Link from 'next/link'
 
 function MyApp({ Component, pageProps }) {
   const [chain, setChain] = useState('')
+  const [account, setAccount] = useState('')
 
 
   useEffect(() => {
     checkConnectedChain()
-  }, [chain])
+    checkConnectedAccount()
+  }, [chain, account])
 
   const checkConnectedChain = async () => {
-    // try {
-    //   await window.ethereum.request({
-    //     method: "eth_requestAccounts",
-    //   });
-    //   setChain({
-    //     accounts: await ethereum.request({ method: "eth_requestAccounts" })
-    //   })
-    //   console.log(chain)
-    // } catch (error) {
-    // }
     setChain(await String(window.ethereum.chainId))
     console.log(chain)
+  }
+
+  const checkConnectedAccount = async () => {
+    try {
+      await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      // setAccount({
+      //   accounts: await ethereum.request({ method: "eth_requestAccounts" })
+      // })
+      let userAccounts = await ethereum.request({ method: "eth_requestAccounts" })
+      console.log("userAccounts:")
+      console.log(userAccounts)
+      setAccount(userAccounts[0])
+    } catch (error) {
+    }
+    console.log("account state:")
+    console.log(account)
   }
 
   const switchNetworkMumbai = async () => {
@@ -62,20 +72,27 @@ function MyApp({ Component, pageProps }) {
   return (
     <div>
       <p className="text-4xl p-3 font-bold">Metaverse Marketplace</p>
-      {chain === '0x89'?
+      {chain === '0x89' && account!= ''?
         // <button onClick={switchNetworkMumbai} className="font-bold mb-2 mr-2 float-right bg-pink-500 text-white rounded p-3 shadow-lg">
-        // connected
+        //    {account &&
+        //     `${account.slice(0, 6)}...${account.slice(
+        //       account.length - 4,
+        //       account.length
+        //     )}`}
         // </button>
-        console.log("connected")
+        <p className="font-bold mb-2 mr-2 float-right text-black rounded p-3 shadow-lg">
+        connected: {account} 
+        {/* {account &&
+         `${account.slice(0, 6)}...${account.slice(
+           account.length - 4,
+           account.length
+         )}`} */}
+     </p>
         :
         <button onClick={switchNetworkMumbai} className="font-bold mb-2 float-right bg-pink-500 text-white mr-3 rounded p-3 shadow-lg">
             connect to Polygon
         </button>
-      }
-    {/* <button onClick={switchNetworkMumbai} className="font-bold mb-2 float-right bg-pink-500 text-white rounded p-3 shadow-lg">
-          connect to Polygon
-      </button> */}
-      
+      }    
       <nav className="border-b p-3">
         <div className="flex mt-4">
           <Link href="/">
@@ -83,17 +100,17 @@ function MyApp({ Component, pageProps }) {
               Marketplace
             </a>
           </Link>
-          <Link href="/create-item">
+          <Link href="./create-item">
             <a className="mr-6 text-pink-500">
               Mint to Sell
             </a>
           </Link>
-          <Link href="/create-personal-item">
+          <Link href="./create-personal-item">
             <a className="mr-6 text-pink-500">
               Mint to Own
             </a>
           </Link>
-          <Link href="/my-assets">
+          <Link href="./my-assets">
             <a className="mr-6 text-pink-500">
               My NFTs
             </a>
