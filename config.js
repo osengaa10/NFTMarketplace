ec5c3634159edd3691427714d874aed865f8c7ab
Austin <osengaa10@icloud.com>
2021-11-05 17:26:10 -0400

    mainnet

--- config.js
+++ config.js
@@ -4,14 +4,14 @@
 // export const nftccaddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
 // export const contractfactorynftaddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
 
-// Polygon mumbai
-export const nftmarketaddress = "0x02B5BdCb27efBc11e58C616050AB5EF4d5C00CC4"
-export const nftaddress = "0x77235BF8E124E23fA9ca7e6d5957b4928E0E7acC"
-export const nftccaddress = "0x299D5ff3bD4cE65C4268A0F8c7Ff6933c3517Aa5"
-export const contractfactorynftaddress = "0x4176d7E496e4fCfc20AfA7dDD947EC3e67083600"
+// // Polygon mumbai
+// export const nftmarketaddress = "0x02B5BdCb27efBc11e58C616050AB5EF4d5C00CC4"
+// export const nftaddress = "0x77235BF8E124E23fA9ca7e6d5957b4928E0E7acC"
+// export const nftccaddress = "0x299D5ff3bD4cE65C4268A0F8c7Ff6933c3517Aa5"
+// export const contractfactorynftaddress = "0x4176d7E496e4fCfc20AfA7dDD947EC3e67083600"
 
-// // Polygon mainnet
-// export const nftmarketaddress = "0xC708282ebE5E7895dC6119a2d8Cf3FFC7D97F04c"
-// export const nftaddress = "0x80A37F192d5f1A229C06391f479cE25DA4F5DCE6"
-// export const nftccaddress = "0x666A6bd9E14A8e009aAea958ffdf7786620CA6Ec"
-// export const contractfactorynftaddress = "0x88805df62e75Fe16Dd7E06CBfcd2e4c51F782fb2"
+// Polygon mainnet
+export const nftmarketaddress = "0xf9bCA1a8272593D4e148D80809d5d52b3c35Cea1"
+export const nftaddress = "0xbCaD73926326dc8E1B05aE56D9eDdD929eDE2daB"
+export const nftccaddress = "0xa765083f19E1b06eC96D36DaA29BE284f0dcb1b4"
+export const contractfactorynftaddress = "0x9B2A6C294f4b98E9A4D620e179408407E4bAbF57"
